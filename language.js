d9af083c066417c96ff5b6cc3b132cc428ac285c
ifgy <65743189+ifgy@users.noreply.github.com>
2022-01-17 16:22:56 +0100

    init

---
+++ language.js
@@ -0,0 +1,58 @@
+const text = {
+    "index_header" : [
+        "<p class='phone'>MON_PORTFOLIO</p><nav class='desktop'><a href='portfolio.html'>MON_PORTFOLIO</a><br><a href='about.html'>QUI_SUIS_JE</a><br><a href='contact.html'>ME_CONTACTER</a></nav>",
+        "<p class='phone'>MY_PORTFOLIO</p><nav class='desktop'><a href='portfolio.html'>MY_PORTFOLIO</a><br><a href='about.html'>ABOUT_ME</a><br><a href='contact.html'>CONTACT_ME</a></nav>"
+    ],
+    "python" : [
+        "<p>Ce jeu 2D est la réalisation de mon projet final de ma dernière année de lycée, option Informatique et Sciences du numérique.</p><p>Cette réalisation est le résultat de mes premiers mois d'apprentissage de la programmation avec python.</p><p>J'ai développé un jeu similaire au vintage \"space game\". Comme vous pourrez le voir, je me suis inspiré de l'univers d'un anime, \"Attaque des titans\", pour créer le pixel design de ce projet.</p><p style='color: #cec217;''>LANGAGE : Python</p>",
+        "<p>This 2D game is the final project of my senior year in highschool for the Computer and Digital Science class.</p><p>This is the result of several months learning for the first time programmation.</p><p>I developped a game similar to the vintage space game. As you can see, I got inspired by the universe of an anime called \"Attack on titan\" to create the pixel design of this project.</p><p style='color: #cec217;'>USED : Python</p>"
+    ],
+    "horizon" : [
+        "<p>Ce site est la réalisation de mon projet tutoré de première année de DUT Métier du Multimédia et de l'Internet.</p><p>J'ai tenu le rôle de développeuse Full-Stack dans une équipe de quatre membres pour développer un site aux caractéristiques d'un jeu. Ce jeu permet à l'utilisateur de découvrir la ville Horizon, en utilisant les touches directionnelles, tout en apprenant plus sur les débouchés post-dut MMI.</p><p style='color: #3ea59c;'>LANGAGES : PHP, SQL, JQUERY, CSS, HTML</p>",
+        "<p>This website was created as my first year tutored project for my Multimedia and Internet profession diploma.</p> <p>I worked as the Full-stack developer in a team of 4 people in order to create an informative website. It has the caracteristics of a small game as you move by using the keyboard's directional arrows. You discover the Horizon city and multiple professions post training.</p><p style='color: #3ea59c;'>USED : PHP, SQL, JQUERY, CSS, HTML</p>"
+    ],
+    "blog" : [
+        "<p>Ce site a été réalisé dans le cadre du cours de Développement web de mon DUT Métiers du Multimédia et de l'Internet.</p><p>En binôme, nous avons développé un simple blog avec une interface back pour un admin.<br>Certaines fonctionnalités incluses sont&nbsp;: inscription/connexion/déconnexion, accès sans compte, être connecté pour écrire un commentaire, accès à l'interface admin avec les identifiants admin...</p><p style='color: #98c959;'>USED : PHP, SQL, JS, CSS, HTML</p>",
+        "<p>This website was created as a school project for the Web Development class.</p><p>In pairs, we developped a small blog-like website with a back-end interface for an admin.<br>Some functionalities included are&nbsp;: sign in/log in/log out, access the blog without an account, write a comment if connected, access the back interface if you are the admin, remove comments, users and article, write an article.</p><p style='color: #98c959;'>USED : PHP, SQL, JS, CSS, HTML</p>"
+    ],
+    "hexword" : [
+        "<p>Ce site a été réalisé dans le cadre du cours d'Intégration Web de mon DUT Métiers du Multimédia et de l'Internet.</p><p>L'objectif était de combiner une couleur héxadécimal avec le mot qu'il constitue.</p><p>J'ai choisi la couleur/mot BEE pour créer un jeu cliquable.</p><p style='color: #bd5c7c;'>LANGAGES : D3JS, CSS, HTML</p><p style='color: #bd5c7c;'>LIEN : <a href='https://ifgy.github.io/hexword/' target='_blank'>https://ifgy.github.io/hexword/</a></p>",
+        "<p>This website was created as a small school project for the Web Integration class.</p><p>The aim was to combine the color in hexadecimal and the word coming out of it.</p><p>I used the color/word BEE and made a clicker game.</p><p style='color: #bd5c7c;'>USED : D3JS, CSS, HTML</p><p style='color: #bd5c7c;'>LINK : <a href='https://ifgy.github.io/hexword/' target='_blank'>https://ifgy.github.io/hexword/</a></p>"
+    ],
+    "shooter" : [
+        "<p>Ce jeu est le résultat d'un projet dans le cadre du cours de Programmation Objet Orienté de mon DUT Métiers du Multimédia et de l'Internet.</p><p>En binôme, nous avons développé un jeu similaire au jeu vintage \"space game\".</p><p style='color: #4c7fc2;'>LANGAGES : D3JS, JS, CSS, HTML</p><p style='color: #4c7fc2;'>LIEN : <a href='https://ifgy.github.io/Shoot/' target='_blank'>https://ifgy.github.io/Shoot/</a></p>",
+        "<p>This game was created as a project for the Object-oriented programming class.</p><p>In group of pairs, we developped this shooting game, similar to the vintage space game.</p><p style='color: #4c7fc2;'>USED : D3JS, JS, CSS, HTML</p><p style='color: #4c7fc2;'>LINK : <a href='https://ifgy.github.io/Shoot/' target='_blank'>https://ifgy.github.io/Shoot/</a></p>"
+    ],
+    "vivide" : [
+        "<p>Ce site est la réalisation de mon projet tutoré de seconde année de DUT Métier du Multimédia et de l'Internet.</p><p>J'ai tenu le rôle de développeuse Full-stack dans une équipe de 4 membres pour créer un site d'apprentissage en ligne. Le contenu de la plateforme ont été créé par l'équipe VIVIDE.</p><p style='color: #8c3cac;'>LANGAGES : PHP, SQL, REACTJS, CSS, HTML</p><p style='color: #8c3cac;'>LIEN : <a href='https://www.vivide.app/' target='_blank'>https://www.vivide.app/</a></p>",
+        "<p>This website was created as my second year tutored project for my Multimedia and Internet profession diploma.</p><p>I worked as the Full-stack developer in a team of 4 people in order to create an online education platform. The content of the platform was entirely made by our team.</p><p style='color: #8c3cac;'>USED : PHP, SQL, REACTJS, CSS, HTML</p><p style='color: #8c3cac;'>LINK : <a href='https://www.vivide.app/' target='_blank'>https://www.vivide.app/</a></p>"
+    ],
+    "about_title" : [
+        "QUI_SUIS_JE",
+        "ABOUT_ME"
+    ],
+    "about_p" : [
+        "Bonjour&nbsp;! Je suis Léa Nguyen.<br>Résoudre des problèmes et développer des projets me stimule. C'est pourquoi j'aime apprendre continuellement à propos de tout, ce qui me rend polyvalente.<br> A propos de mes passe-temps, j'aime la musique, l'art, l aprogrammation, coudre, broder, lire, apprendre et améliorer les langues parlées, essayer de nouveaux sports...",
+        "Hi! I am Léa Nguyen.<br>I am stimulated by resolving problems and developping projects. This explains why I enjoy learning constantly and about anything, which makes me a versatile person.<br> About my hobbies, I enjoy music, art, programming, sewing, embroidery, reading, learning and improving my speaking languages, trying new physical activities, .... and so on."
+    ],
+    "skills" : [
+        "<li>Autodidacte</li><li>Collaborer</li><li>Résoudre</li><li>Adaptation</li><li>Gestion du stress</li><li>Organisée</li>",
+        "<li>Self-learner</li><li>Collaboration</li><li>Problem solving</li><li>Adaptability</li><li>Stress management</li><li>Organized</li>"
+    ],
+    "cv" : [
+        "<a href='medias/cv.pdf' target='_blank'>LIRE MON CV<img src='medias/french.png' alt=''></a>",
+        "<a href='medias/cv.pdf' target='_blank'>LOOK AT MY CV<img src='medias/french.png' alt=''></a>"
+    ],
+    "footer_nav" : [
+        "<a href='portfolio.html'>MON_PORTFOLIO</a><a href='about.html'>QUI_SUIS_JE</a><a href='contact.html'>ME_CONTACTER</a>",
+        "<a href='portfolio.html'>MY_PORTFOLIO</a><a href='about.html'>ABOUT_ME</a><a href='contact.html'>CONTACT_ME</a>"
+    ],
+    "contact_title" : [
+        "ME_CONTACTER",
+        "CONTACT_ME"
+    ],
+    "copy" : [
+        "Copier",
+        "Copy_me"
+    ]
+}
