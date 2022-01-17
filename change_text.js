3928771b1bce2072fda845262ca53b70744f3a4b
ifgy <65743189+ifgy@users.noreply.github.com>
2022-01-17 17:50:11 +0100

    init

---
+++ change_text.js
@@ -0,0 +1,16 @@
+let lang = sessionStorage.getItem('lang');
+if (lang == null) {
+    change_text(0);
+} else {
+    change_text(lang);
+}
+fr_button = () => {
+    sessionStorage.setItem('lang', 0);
+    lang = sessionStorage.getItem('lang');
+    change_text(lang);
+}
+eng_button = () => {
+    sessionStorage.setItem('lang', 1);
+    lang = sessionStorage.getItem('lang');
+    change_text(lang);
+}
